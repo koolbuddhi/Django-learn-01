448f3bfed95705960e3bb624eadd15e7c182b233
Buddhima Wickramasinghe <buddhi@bistecglobal.com>
2022-02-09 18:16:28 +0530

    custom admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
